46ebf9c8523c100d85efa826b84ff6b032df6e7a
shizuku86 <shizuku86710@gmail.com>
2025-08-04 14:53:23 +0900

    hasNameConflict(), hasMissingRequirements()

--- src/router/init/behaviorInitializeValidate.ts
+++ src/router/init/behaviorInitializeValidate.ts
@@ -9,7 +9,7 @@ import { BehaviorInitializePending } from "./behaviorInitializePending";
  * can be registered in Register.
  */
 export class BehaviorInitializeValidate {
-    private static validatedAddons: AddonProperty[];
+    private static validatedAddons: AddonProperty[] = [];
 
     static validatePendingAddons(): void {
         const pendingAddons = BehaviorInitializePending.getAll();
@@ -20,7 +20,18 @@ export class BehaviorInitializeValidate {
     }
 
     private static validateAddonProperty(addonProperty: AddonProperty): boolean {
+        if (this.hasNameConflict(addonProperty)) return false;
+        if (this.hasMissingRequirements(addonProperty)) return false;
+        
         return true;
+    }
+
+    private static hasNameConflict(addon: AddonProperty): boolean {
+        return false;
+    }
+
+    private static hasMissingRequirements(addon: AddonProperty): boolean {
+        return false;
     }
 
     static get(): AddonProperty[] {
