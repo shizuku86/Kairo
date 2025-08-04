176af8afb33a87886155eed739407529a9d8a4ec
shizuku86 <shizuku86710@gmail.com>
2025-08-04 14:24:04 +0900

    Designed an abstract class

--- src/router/init/behaviorInitializeValidate.ts
+++ src/router/init/behaviorInitializeValidate.ts
@@ -1,3 +1,6 @@
+import type { AddonProperty } from "../AddonProperty";
+import { BehaviorInitializePending } from "./behaviorInitializePending";
+
 /**
  * BehaviorInitializePendingに仮置きされたアドオンを、
  * Registerで登録しても良いかどうかを検証するためのクラス
@@ -6,5 +9,21 @@
  * can be registered in Register.
  */
 export class BehaviorInitializeValidate {
+    private static validatedAddons: AddonProperty[];
 
+    static validatePendingAddons(): void {
+        const pendingAddons = BehaviorInitializePending.getAll();
+
+        this.validatedAddons = pendingAddons.filter(addonProperty => {
+            return this.validateAddonProperty(addonProperty);
+        });
+    }
+
+    private static validateAddonProperty(addonProperty: AddonProperty): boolean {
+        return true;
+    }
+
+    static get(): AddonProperty[] {
+        return this.validatedAddons;
+    }
 }
